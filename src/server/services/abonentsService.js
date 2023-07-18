8355684faeed5cc1464d4f9ad16a86d898c71a0a
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-18 11:30:34 +0300

    module abonentsService finished for switchOn & switchOff functions (with tests)

--- src/server/services/abonentsService.js
+++ src/server/services/abonentsService.js
@@ -1,17 +1,39 @@
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
+const { spawn } = require('child_process')
+
+function ipfw(args) {
+  const child = spawn('/sbin/ipfw', args)
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
+  })
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`)
+  })
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`)
+  })
+
+  return child
+}
 
 module.exports.switchOff = async function (abonentId, ipAddress, vlanId) {
 
-  const deleteCommand = `/sbin/ipfw delete ${abonentId}`
-  const addCommand = `/sbin/ipfw add ${abonentId} deny ip from ${ipAddress}/32 to any via vlan${vlanId} in`
-
   try {
-    await exec(deleteCommand)
-    console.log(`Deleted rule ${abonentId} successfully.`)
+    const deleteCommand = ipfw(['delete', abonentId])
+    deleteCommand.on('close', (code) => {
+      if (code === 0) {
+        console.log(`Deleted rule ${abonentId} successfully.`)
+      }
+    })
 
-    await exec(addCommand)
-    console.log(`Added rule ${abonentId} successfully.`)
+    const addCommand = ipfw(['add', abonentId, 'deny', 'ip', 'from', `${ipAddress}/32`, 'to', 'any', 'via', `vlan${vlanId}`, 'in'])
+    addCommand.on('close', (code) => {
+      if (code === 0) {
+        console.log(`Added rule ${abonentId} successfully.`)
+      }
+    })
 
     return true
   } catch (error) {
@@ -19,3 +41,22 @@ module.exports.switchOff = async function (abonentId, ipAddress, vlanId) {
     return false
   }
 }
+
+module.exports.switchOn = async function (abonentId) {
+
+  try {
+    const deleteCommand = ipfw(['delete', abonentId])
+    deleteCommand.on('close', (code) => {
+      if (code === 0) {
+        console.log(`Deleted rule ${abonentId} successfully.`)
+      }
+    })
+    return true
+  } catch (error) {
+    console.error('Error executing commands:', error)
+    return false
+  }
+}
+
+//await abonentsService.switchOff('9999', '192.168.199.199', '199')  //temprorary for testing
+//await abonentsService.switchOn('9999')  //temprorary for testing
