bd5ed8546d936083a82b7cfaad845a7239d1281e
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-23 18:13:19 +0300

    Added HOST and PORT to server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const { app, redirectServer } = require('./index')
+const HOST = process.env.HOST || '127.0.0.1'
 
-app.listen({ port: process.env.PORT || 8080 }, (err, address) => {
+app.listen({ port: process.env.PORT || 8080, host: HOST }, (err, address) => {
   if (err) {
     app.log.error(err)
     process.exit(1)
@@ -10,7 +11,7 @@ app.listen({ port: process.env.PORT || 8080 }, (err, address) => {
   console.log(`[API] Service listening on ${address}`)
 })
 
-redirectServer.listen({ port: process.env.PORT_FOR_REDIRECT || 8081 }, (err, address) => {
+redirectServer.listen({ port: process.env.PORT_FOR_REDIRECT || 8081, host: HOST }, (err, address) => {
   if (err) {
     redirectServer.log.error(err)
     console.error(err)
