a52fe984daa54caa8e3c523ed098b34d0df2ce8a
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-23 11:14:37 +0300

    Fix the error in the bash script where the mac addresses gathering

--- src/bashForSaving/99_mac_ip_save.sh
+++ src/bashForSaving/99_mac_ip_save.sh
@@ -1,1 +1,1 @@
-arp -a | awk '!/incomplete/{ print $2 "\t" $4 "\t" $6 "\t" $7 }' | sed 's/[()]//g' | sort > /usr/home/ftp1c/macs.log
+arp -a | awk '\!/incomplete/{ print $2 "\t" $4 "\t" $6 "\t" $7 }' | sed 's/[()]//g' | sort > /usr/home/ftp1c/macs.log
