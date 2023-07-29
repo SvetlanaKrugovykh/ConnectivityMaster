5f4938a0c7c52b7693d3096ae532c1e9c64d182f
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-29 11:57:25 +0300

    some remarks

--- src/bashForSaving/bkgnd-start-prg.sh
+++ src/bashForSaving/bkgnd-start-prg.sh
@@ -1,4 +1,4 @@
-cd \intelligence\CM-SERVER\ConnectivityMaster\
+cd /intelligence/CM-SERVER/ConnectivityMaster/
 pwd
 ### npm install -g pm2
 pm2 start ./src/server/server.js --watch --name "Obuhiv-CM-Server"
