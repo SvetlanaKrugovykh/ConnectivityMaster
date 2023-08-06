c5fbafc5019447c5f9520745aabe178dd0b3da36
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-08-06 12:18:54 +0300

    fixing the error when switchOn processing *before the testing

--- src/server/services/abonentsService.js
+++ src/server/services/abonentsService.js
@@ -52,17 +52,32 @@ module.exports.switchOn = async function (abonentId) {
 
   try {
     const deleteCommand = ipfw(['delete', abonentId])
-    deleteCommand.on('close', (code) => {
-      if (code === 0) {
-        console.log(`${new Date()}: Deleted rule ${abonentId} successfully.`)
-      }
+
+    await new Promise((resolve, reject) => {
+      deleteCommand.on('exit', (code) => {
+        if (code === 0) {
+          console.log(`${new Date()}: Deleted rule ${abonentId} successfully.`)
+          resolve()
+        } else {
+          const errorMessage = `Error deleting rule ${abonentId}, exit code: ${code}`
+          console.error(`${new Date()}: ${errorMessage}`)
+          reject(new Error(errorMessage))
+        }
+      })
+
+      deleteCommand.on('error', (error) => {
+        console.error(`${new Date()}: Error executing delete command:`, error)
+        reject(error)
+      })
     })
+
     return true
   } catch (error) {
-    console.error(`${new Date()}:Error executing commands:`, error)
+    console.error(`${new Date()}: Error executing commands:`, error)
     return false
   }
 }
+
 
 //await abonentsService.switchOff('9999', '192.168.199.199', '199')  //temprorary for testing
 //await abonentsService.switchOn('9999')  //temprorary for testing
