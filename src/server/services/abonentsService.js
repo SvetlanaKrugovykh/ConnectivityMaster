9419d5bc1f976e525eec1a3b6d2a43f2752b4a90
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-08-10 14:59:19 +0300

    fix the algorithm of the abonentsService.js

--- src/server/services/abonentsService.js
+++ src/server/services/abonentsService.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process')
+const { get } = require('http')
 
 function ipfw(args) {
   const child = spawn('/sbin/ipfw', args)
@@ -22,19 +23,19 @@ module.exports.switchOff = async function (abonentId, ipAddress, vlanId) {
   if (process.env.PLATFORM !== 'freebsd') {
     return true
   }
-
+  const abonentIdТForRules = getAbonentIdТForRules(abonentId)
   try {
-    const deleteCommand = ipfw(['delete', abonentId])
+    const deleteCommand = ipfw(['delete', abonentIdТForRules])
     deleteCommand.on('close', (code) => {
       if (code === 0) {
-        console.log(`${new Date()}: Deleted rule ${abonentId} successfully.`)
+        console.log(`${new Date()}: Deleted rule ${abonentId} -> ${abonentIdТForRules} successfully.`)
       }
     })
 
-    const addCommand = ipfw(['add', abonentId, 'deny', 'ip', 'from', `${ipAddress}/32`, 'to', 'any', 'via', `vlan${vlanId}`, 'in'])
+    const addCommand = ipfw(['add', abonentIdТForRules, 'deny', 'ip', 'from', `${ipAddress}/32`, 'to', 'any', 'via', `vlan${vlanId}`, 'in'])
     addCommand.on('close', (code) => {
       if (code === 0) {
-        console.log(`${new Date()}: Added rule ${abonentId} successfully.`)
+        console.log(`${new Date()}: Added rule ${abonentId} -> ${abonentIdТForRules} successfully.`)
       }
     })
 
@@ -49,17 +50,17 @@ module.exports.switchOn = async function (abonentId) {
   if (process.env.PLATFORM !== 'freebsd') {
     return true
   }
-
+  const abonentIdТForRules = getAbonentIdТForRules(abonentId)
   try {
-    const deleteCommand = ipfw(['delete', abonentId])
+    const deleteCommand = ipfw(['delete', abonentIdТForRules])
 
     await new Promise((resolve, reject) => {
       deleteCommand.on('exit', (code) => {
         if (code === 0) {
-          console.log(`${new Date()}: Deleted rule ${abonentId} successfully.`)
+          console.log(`${new Date()}: Deleted rule ${abonentId} ->  {abonentIdТForRules} successfully.`)
           resolve()
         } else {
-          const errorMessage = `Error deleting rule ${abonentId}, exit code: ${code}`
+          const errorMessage = `Error deleting rule ${abonentId} -> ${abonentIdТForRules}, exit code: ${code}`
           console.error(`${new Date()}: ${errorMessage}`)
           reject(new Error(errorMessage))
         }
@@ -78,6 +79,19 @@ module.exports.switchOn = async function (abonentId) {
   }
 }
 
-
+function getAbonentIdТForRules(abonentId) {
+  const nAbonentId = Number(abonentId)
+  let bgnr = ''
+  if (nAbonentId < 1000) {
+    bgnr = '01'
+  } else if (nAbonentId >= 1000 && nAbonentId < 2000) {
+    bgnr = '02'
+  } else if (nAbonentId >= 2000 && nAbonentId < 3000) {
+    bgnr = '03'
+  } else if (nAbonentId >= 3000 && nAbonentId < 4000) {
+    bgnr = '04'
+  }
+  return `${bgnr}${abonentId}`
+}
 //await abonentsService.switchOff('9999', '192.168.199.199', '199')  //temprorary for testing
 //await abonentsService.switchOn('9999')  //temprorary for testing
