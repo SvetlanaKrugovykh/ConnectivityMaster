4f8312fa54b170cb13cd89ef9ec0eb1ee166ac0a
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-12-06 09:59:07 +0100

    add console.log for crossover requests

--- src/server/plugins/redirect.auth.plugin.js
+++ src/server/plugins/redirect.auth.plugin.js
@@ -6,6 +6,18 @@ const allowedSubnets = process.env.REDIRECT_ALLOWED_SUBNETS.split(',')
 
 const restrictIPSubnetMiddleware = (req, reply, done) => {
   const clientIP = req.ip
+  const CROSSOVER_IP = process.env.CROSSOVER_IP
+
+  if (clientIP === CROSSOVER_IP) {
+    const { body, url } = req
+    const bodySnippet = body ? JSON.stringify(body).substring(0, 35) : 'No request body'
+    const urlSnippet = url ? url.substring(0, 35) : 'No URL'
+
+    console.log(`${new Date()}: Client IP matches CROSSOVER_IP: ${clientIP}`)
+    console.log(`First 35 characters of req.body: ${bodySnippet}`)
+    console.log(`First 35 characters of req.url: ${urlSnippet}`)
+  }
+
   if (!ipRangeCheck(clientIP, allowedSubnets)) {
     console.log(`${new Date()}: Client IP is Forbidden: ${clientIP}`)
     reply.code(403).send('Forbidden')
