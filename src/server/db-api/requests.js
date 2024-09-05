3eea075e448c965c38c059819bd2609d91d11dd7
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-09-05 14:56:44 +0200

    some changes

--- src/server/db-api/requests.js
+++ src/server/db-api/requests.js
@@ -1,5 +1,23 @@
 const axios = require('axios')
 
 module.exports.updatePayment = async function (paymentData) {
+  const DB_UPDATE_URL = process.env.DB_UPDATE_URL
+  const signature = process.env.DB_UPDATE_SIGNATURE || ''
+
+  const response = await axios({
+    method: 'post',
+    url: DB_UPDATE_URL,
+    responseType: 'string',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      paymentData
+    },
+    signature
+  })
+  if (!response.status == 200) {
+    return null
+  }
 
 }
